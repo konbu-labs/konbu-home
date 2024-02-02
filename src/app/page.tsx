b8e06cc62a28f43a68d48daa50a629fae40e1ff6
kumentle <kumentle@gmail.com>
2024-02-02 09:53:31 +0000

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
+import TopHeader from '@/components/top-header'
 import { Button } from '@/components/ui/button'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button>Click</Button>
+    <main className='flex flex-col items-center justify-center min-h-screen'>
+      <TopHeader />
+      <div className='flex flex-col items-center gap-8 flex-1 justify-center'>
+        <span className='text-6xl flex font-bold bg-clip-text text-transparent bg-gradient animate-gradient bg-[-100%] bg-[length:300%_300%]'>
+          Decentralized Perpetual Exchange
+        </span>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button className='text-2xl py-6 text-white'>Launch App</Button>
+          </PopoverTrigger>
+          <PopoverContent className='w-auto bg-white'>
+            <div className='grid text-black font-medium'>Coming Soon</div>
+          </PopoverContent>
+        </Popover>
+      </div>
     </main>
   )
 }
---
+++ src/components/top-header.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import { FaDiscord } from 'react-icons/fa'
+
+export default function TopHeader() {
+  return (
+    <header className='sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+      <div className='container flex h-14 max-w-screen-2xl items-center'>
+        <div className='mr-4 hidden md:flex'>
+          <a
+            className='mr-6 flex items-center space-x-2'
+            href='/'
+          >
+            <Image
+              src='/konbu.svg'
+              alt='logo'
+              width={20}
+              height={20}
+            ></Image>
+            <span className='hidden font-bold sm:inline-block'>konbu</span>
+          </a>
+        </div>
+        <div className='flex flex-1 items-center justify-between space-x-2 md:justify-end'>
+          <nav className='flex items-center'>
+            <a
+              target='_blank'
+              rel='noreferrer'
+              href='https://twitter.com/shadcn'
+            >
+              <div className='inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0'>
+                <svg
+                  className='h-3 w-3 fill-current'
+                  height='23'
+                  viewBox='0 0 1200 1227'
+                  width='23'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z'></path>
+                </svg>
+                <span className='sr-only'>Twitter</span>
+              </div>
+            </a>
+
+            <a>
+              <FaDiscord size={20} />
+            </a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
